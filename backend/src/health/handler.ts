bf8809349de31912533740dfa0914cbef60817c4
dadlez <lukaszdadlez@gmail.com>
2026-03-15 14:02:37 +0100

    fix(backend): connection to database + added reading .env

--- backend/src/health/handler.ts
+++ backend/src/health/handler.ts
@@ -5,9 +5,10 @@ export const healthHandler = (db: Database): RouteHandler  => {
   return async () => {
     try {
       await db.query('SELECT 1')
-      console.log('SUCCESS')
       return { status: 'ok', db: 'ok' }
-    } catch {
+    } catch (error) {
+      // TODO: added logger
+      console.error(error);
       return { status: 'ok', db: 'error' }
     }
   }
