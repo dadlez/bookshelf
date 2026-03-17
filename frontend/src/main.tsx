a4c50b905e4f827f68741dd57b0a92964a183517
dadlez <lukaszdadlez@gmail.com>
2026-03-17 10:31:27 +0100

    feat(frontend): added nuqs adapter for storing table settings in url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CssBaseline } from "@mui/material";
+import { NuqsAdapter } from "nuqs/adapters/react";
 import App from "./App";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <App />
-    </QueryClientProvider>
+    <NuqsAdapter>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <App />
+      </QueryClientProvider>
+    </NuqsAdapter>
   </StrictMode>
 );
